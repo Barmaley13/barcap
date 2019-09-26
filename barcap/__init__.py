0b4ebdc9564a23274505a58e4011ff6dacbcb88a
Kirill Belyayev <kbelyayev@gmail.com>
2019-09-26 14:18:28 -0600

    Fixing version problem

--- barcap/__init__.py
+++ barcap/__init__.py
@@ -6,5 +6,5 @@ Trying to capture any barcode with web camera
 from .capture import BarcodeCapture
 
 __author__ = 'Kirill V. Belyayev'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -3,17 +3,33 @@ Packaging this baby...
 """
 
 import os
+import re
+import codecs
 from setuptools import setup, find_packages
 
-from barcap import __version__
-import barcap.device_list.setup as dev_setup
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(base_path, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 if __name__ == '__main__':
     # Add device setup only on Windows
     dep_links = []
     if os.name == 'nt':
-        dev_path = os.path.dirname(os.path.abspath(dev_setup.__file__))
+        dev_path = os.path.join(base_path, 'barcap', 'device_list')
         dep_links.append(f'file:/{dev_path}')
 
     # Add our standard setup
@@ -29,7 +45,7 @@ if __name__ == '__main__':
 
     setup(
         name='barcap',
-        version=__version__,
+        version=find_version('barcap', '__init__.py'),
         description='Extract any barcode using your web camera',
         # long_description=readme,
         # long_description_content_type='text/markdown',
