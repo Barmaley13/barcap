19546169c5b6b0629f16d4068d70c1a36d5ecded
Kirill Belyayev <kbelyayev@gmail.com>
2019-09-26 14:24:01 -0600

    Fixing package distribution problems

--- barcap/__init__.py
+++ barcap/__init__.py
@@ -6,5 +6,5 @@ Trying to capture any barcode with web camera
 from .capture import BarcodeCapture
 
 __author__ = 'Kirill V. Belyayev'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __license__ = 'MIT'
